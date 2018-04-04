5e71d5a6d7cd741c90d2e0c31e6bc0b0d97f4ff1
Muffin <marvin.dick96@web.de>
2018-04-04 16:21:59 +0200

    Added simple Footer

---
+++ src/Footer/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './footer.css';
+
+export class Footer extends React.Component {
+
+    render() {
+        return (
+        <footer class="container-fluid bg-4 text-center" id="myFooter">
+            <p>Example Footer made by Marvin Dick</p>
+        </footer>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ import registerServiceWorker from './registerServiceWorker';
 import {NavBar} from './NavBar/navbar';
 import {Concert} from './Concert/concert';
 import {Featured} from './Featured/featured';
+import {Footer} from "./Footer/footer";
 
 ReactDOM.render(<NavBar />, document.getElementById('navbar'));
 ReactDOM.render(<Featured source={process.env.PUBLIC_URL + '/concerts.json'}/>, document.getElementById('featured'));
 ReactDOM.render(<Concert source={process.env.PUBLIC_URL + '/concerts.json'}/>, document.getElementById('concert'));
+ReactDOM.render(<Footer/>, document.getElementById('footer'));
 registerServiceWorker();
